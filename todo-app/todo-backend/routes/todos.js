1d5a9f2c2def0a640bbc30c2cada3d53342e5405
simon greenwood <simonrobertgreenwood@gmail.com>
2023-05-08 19:27:09 +0100

    12.7 done

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,23 +1,41 @@
-const express = require('express');
-const { Todo } = require('../mongo')
-const router = express.Router();
+const express = require("express")
+const { Todo } = require("../mongo")
+const router = express.Router()
 
 /* GET todos listing. */
-router.get('/', async (_, res) => {
+router.get("/", async (_, res) => {
   const todos = await Todo.find({})
-  res.send(todos);
-});
+  res.send(todos)
+})
 
 /* POST todo to listing. */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
+    done: false,
   })
-  res.send(todo);
-});
+  res.send(todo)
+})
 
-const singleRouter = express.Router();
+/* GET specific todo */
+router.get("/:id", async (req, res) => {
+  const id = req.params.id
+  const todo = await Todo.findOne({ _id: id })
+  res.send(todo)
+})
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id
+  const { text, done } = req.body
+  console.log(text, done)
+  const updatedTodo = await Todo.findByIdAndUpdate(
+    id,
+    { text, done },
+    { new: true }
+  )
+  res.send(updatedTodo)
+})
+const singleRouter = express.Router()
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
@@ -28,22 +46,21 @@ const findByIdMiddleware = async (req, res, next) => {
 }
 
 /* DELETE todo. */
-singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
-});
+singleRouter.delete("/", async (req, res) => {
+  await req.todo.delete()
+  res.sendStatus(200)
+})
 
 /* GET todo. */
-singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+singleRouter.get("/", async (req, res) => {
+  res.sendStatus(405) // Implement this
+})
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+singleRouter.put("/", async (req, res) => {
+  res.sendStatus(405) // Implement this
+})
 
-router.use('/:id', findByIdMiddleware, singleRouter)
+router.use("/:id", findByIdMiddleware, singleRouter)
 
-
-module.exports = router;
+module.exports = router
