54796e83895a71c30eae91c2ffc6f4799bb68eea
simon greenwood <simonrobertgreenwood@gmail.com>
2023-05-16 07:52:22 +0100

    12.9 done

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const router = express.Router();
-
-const configs = require('../util/config')
+const express = require("express")
+const router = express.Router()
+const redis = require("../redis")
+const configs = require("../util/config")
 
 let visits = 0
 
 /* GET index data. */
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   visits++
 
   res.send({
     ...configs,
-    visits
-  });
-});
+    visits,
+  })
+})
 
-module.exports = router;
+module.exports = router
