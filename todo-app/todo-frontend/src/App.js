fd96d1b8c7d9d0bcdfd9cc20907446c42e4db842
simon greenwood <simonrobertgreenwood@gmail.com>
2023-05-21 08:03:05 +0100

    fixed docker file for frontend

--- todo-app/todo-frontend/src/App.js
+++ todo-app/todo-frontend/src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import TodoView from './Todos/TodoView'
-
+import "./App.css"
+import TodoView from "./Todos/TodoView"
 function App() {
   return (
     <div className="App">
       <TodoView />
+      {console.log(process.env.REACT_APP_BACKEND_URL)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,5 +1,4 @@
-import axios from 'axios'
-
+import axios from "axios"
 const apiClient = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
 })
